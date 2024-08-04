1782a76f56710ec81f15a534b035c561a987957b
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2024-08-03 22:58:28 -0400

    fix the stuff

--- vite.config.ts
+++ vite.config.ts
@@ -19,8 +19,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': resolve(__dirname, 'src/'),
-            '#': resolve(__dirname, 'WWPPC-site-common/src/'),
-            '#p': resolve(__dirname, 'WWPPC-site-common/public/')
+            '#': resolve(__dirname, 'WWPPC-site-common/src/')
         }
     },
     build: {
