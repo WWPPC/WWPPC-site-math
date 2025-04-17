f92a28c1b3a9f64ac4f532c4397b8b9292094fc2
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2025-04-16 22:59:57 -0700

    eslint useless still

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -2,16 +2,16 @@
 import '@rushstack/eslint-patch/modern-module-resolution'
 
 export default {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
-  rules: {
-    "no-async-promise-executor": "off"
-  }
+    root: true,
+    'extends': [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/eslint-config-typescript'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest'
+    },
+    rules: {
+        "no-async-promise-executor": "off"
+    }
 }
